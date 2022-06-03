0fb299f3a47a37b38316dd50b37d00f089e4805a
Alexander Rodionov <tandav@tandav.me>
2022-06-03 12:34:58 +0300

    add fallback implementation for python < 3.10

--- bisect_find.py
+++ bisect_find.py
@@ -2,6 +2,45 @@ import bisect
 import subprocess
 import abc
 from typing import Any
+import sys
+
+
+if sys.version_info >= (3, 10):
+    bisect_left = bisect.bisect_left
+else:
+    # hardcode fallback
+    # https://github.com/python/cpython/blob/3.10/Lib/bisect.py#L68-99
+    # https://github.com/python/cpython/pull/20556
+    def bisect_left(a, x, lo=0, hi=None, *, key=None):
+        """Return the index where to insert item x in list a, assuming a is sorted.
+        The return value i is such that all e in a[:i] have e < x, and all e in
+        a[i:] have e >= x.  So if x already appears in the list, a.insert(i, x) will
+        insert just before the leftmost x already there.
+        Optional args lo (default 0) and hi (default len(a)) bound the
+        slice of a to be searched.
+        """
+
+        if lo < 0:
+            raise ValueError('lo must be non-negative')
+        if hi is None:
+            hi = len(a)
+        # Note, the comparison uses "<" to match the
+        # __lt__() logic in list.sort() and in heapq.
+        if key is None:
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if a[mid] < x:
+                    lo = mid + 1
+                else:
+                    hi = mid
+        else:
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if key(a[mid]) < x:
+                    lo = mid + 1
+                else:
+                    hi = mid
+        return lo
 
 
 class color:
@@ -38,7 +77,7 @@ class BisectFind(abc.ABC):
         """
         a = self.options
         x = True  # True means bad
-        i = bisect.bisect_left(a, x, key=self.is_bad)
+        i = bisect_left(a, x, key=self.is_bad)
         if i != len(a) and self.is_bad(a[i]) == x:
             print('=' * 100)
             print(f'first bad option is: {a[i]}')
